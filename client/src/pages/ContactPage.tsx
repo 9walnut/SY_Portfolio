1d1ee48cd91379bdfb301b2c8ef4a07b6df40c8d
9walnut <kguho9202@gmail.com>
2024-05-14 22:54:40 +0900

    ✨ Add contage page
    add contact page

--- client/src/pages/ContactPage.tsx
+++ client/src/pages/ContactPage.tsx
@@ -1,12 +1,105 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ContactPage: React.FC = () => {
-  return (
-    <div>
-      <h1>Contact Me</h1>
-      {/* 여기에 연락처 정보와 메시지 양식을 포함하는 컴포넌트 추가 */}
-    </div>
-  );
-};
+interface ContactPageState {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+}
+
+class ContactPage extends Component<{}, ContactPageState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    };
+  }
+
+  handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value } as Pick<
+      ContactPageState,
+      keyof ContactPageState
+    >);
+  };
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { name, email, subject, message } = this.state;
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Subject:", subject);
+    console.log("Message:", message);
+  };
+
+  render() {
+    return (
+      <div>
+        <div style={{ float: "left", marginRight: "20px" }}>
+          <h2>어디사니</h2>
+          <p>서울특별시 성북구.</p>
+          <p>asdfsafa@Aadsfasdfasfd.com</p>
+        </div>
+        <div style={{ float: "left" }}>
+          <h1>Contact Me</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Subject:
+                <input
+                  type="text"
+                  name="subject"
+                  value={this.state.subject}
+                  onChange={this.handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Message:
+                <textarea
+                  name="message"
+                  value={this.state.message}
+                  onChange={this.handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <button type="submit">Send</button>
+            </div>
+          </form>
+        </div>
+        <div style={{ clear: "both" }} />
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
